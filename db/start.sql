e1aa1dee9c748c249a6da73ca750406b275a672f
Nesscafe <darkskull328@gmail.com>
2024-11-20 21:11:27 -0600

    Spark -> PostgreSQL scripts

--- db/start.sql
+++ db/start.sql
@@ -1,34 +1,33 @@
 -- creates database For postgres
-CREATE DATABASE Summaries;
+CREATE DATABASE summaries;
 
-\c Summaries
+\c summaries
 
 
 CREATE TABLE Total_Spent_Per_Customer (
     customer_id VARCHAR PRIMARY KEY,
-    Client_name NVARCHAR,
-    gender NVARCHAR,
-    address_location NVARCHAR,
-    job_title NVARCHAR,
+    Client_name VARCHAR,
+    gender VARCHAR,
+    address_location VARCHAR,
+    job_title VARCHAR,
     total_spent NUMERIC
 );
 
 CREATE TABLE Product_Purchase_Count (
     product_id VARCHAR PRIMARY KEY, 
-    size_of_product NVARCHAR,
-    product_line NVARCHAR,
-    Product_id NVARCHAR,
-    Product_class NVARCHAR,
-    Manufacturer NVARCHAR,   
+    size_of_product VARCHAR,
+    product_line VARCHAR,
+    Product_class VARCHAR,
+    Manufacturer VARCHAR,   
     purchase_count INT
 );
 
 CREATE TABLE Average_Spend_Per_Customer (
     customer_id VARCHAR PRIMARY KEY,
-    Client_name NVARCHAR,
-    gender NVARCHAR,
-    address_location NVARCHAR,
-    job_title NVARCHAR,
+    Client_name VARCHAR,
+    gender VARCHAR,
+    address_location VARCHAR,
+    job_title VARCHAR,
     total_spent NUMERIC,
     transaction_count INT,
     average_spent NUMERIC
@@ -36,10 +35,10 @@ CREATE TABLE Average_Spend_Per_Customer (
 
 CREATE TABLE Transaction_Count_Per_Customer (
     customer_id VARCHAR PRIMARY KEY,
-    Client_name NVARCHAR,
-    gender NVARCHAR,
-    address_location NVARCHAR,
-    job_title NVARCHAR,
+    Client_name VARCHAR,
+    gender VARCHAR,
+    address_location VARCHAR,
+    job_title VARCHAR,
     total_spent NUMERIC,
-    transaction_count INT,
+    transaction_count INT
 );
